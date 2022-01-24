a9656c0bd040e5b447ab737d9a384db47440b8cb
lorenapassagem <lorenapassagem@gmail.com>
2022-01-24 09:32:56 -0300

    criar menu desktop e mobile

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,24 +1,24 @@
 import React, {useState} from "react";
-import MenuLateral from "../MenuLateral";
 import './index.css'
 
-function Menu(){  
-  
+
+function Menu(){    
+  const [clicked, setClicked] = useState(false)
+
   const openMenu = () =>{
-    console.log(`Clicou!`)
-    const [clicked, setClicked] = useState(close)
-    return 
+    console.log("aquuui!")
+    setClicked(!clicked)
   }
   return (
     <>
-      <img className="icon-menu" onClick={openMenu} src='./src/img/menu.png' alt='menu hamburger'></img>
-      <ul className="list-menu">
+      <img onClick={openMenu} className="icon-menu" src='./src/img/menu.png' alt='menu hamburger'></img>
+      <ul className={`list-menu ${clicked ? 'open-menu': 'hide-menu'}`}>
         <li className="list-menu-item"><a href='#'>sobre</a></li>
         <li className="list-menu-item"><a href='#'>hospitais </a></li>      
         <li className="list-menu-item"><a href='#'>benefícios</a></li>
         <li className="list-menu-item"><a href='#'>serviços especiais</a></li>
         <li className="list-menu-item"><a href='#'>contatos </a></li>
-      </ul>  
+      </ul>
     </> 
  )
 }
