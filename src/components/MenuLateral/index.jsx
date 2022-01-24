50dff5a8b2e49c283101e291c16ccaba1c1813c7
lorenapassagem <lorenapassagem@gmail.com>
2022-01-24 09:34:37 -0300

    deletar component menu lateral

--- src/components/MenuLateral/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function MenuLateral({toggleMenu}){
-    return(
-        <div className={`MenuLateral ${isOpen ? 'open-menu':'hide-menu'}`}>menu aqui!</div>
-    )
-}
-export default MenuLateral
