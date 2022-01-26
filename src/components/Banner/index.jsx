05c523cc8190c5db3e54f387b836443e5f6fd5eb
lorenapassagem <lorenapassagem@gmail.com>
2022-01-26 10:11:54 -0300

    criar o component Banner e o css dele

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import './index.css'
 
 function Banner(){
-    return <section className="banner"></section>
+    return <section className="banner">
+        <h2 className="banner-title">
+            Conforto e qualidade para levar 
+            bem-estar com alto padrão de atendimento a seus funcionários.
+        </h2>
+    </section>
 }
 export default Banner
