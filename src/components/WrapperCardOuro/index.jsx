452a0f5e24d32570bfee0c4bb2da7f3c79a1a62a
lorenapassagem <lorenapassagem@gmail.com>
2022-01-26 10:15:21 -0300

    criar components para envolver o component CardOuro

---
+++ src/components/WrapperCardOuro/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CardPlanoOuro from "../CardPlanoOuro";
+
+
+function WrapperCardOuro(){
+    return (
+      <>
+        <CardPlanoOuro text="O plano Ouro é ideal para sua empresa"/>
+      </>    
+    )
+}
+export default WrapperCardOuro
