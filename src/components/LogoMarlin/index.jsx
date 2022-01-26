7a3ee157c7262359860902eb1f3ebddb1b253ec0
lorenapassagem <lorenapassagem@gmail.com>
2022-01-26 15:07:57 -0300

    criar logo da Marlin no footer

---
+++ src/components/LogoMarlin/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './index.css'
+
+function LogoMarlin(){
+    return (
+       <>
+        <div className="logoMarlin">Desenvolvimento:</div>
+      </>  
+    ) 
+}
+
+export default LogoMarlin
