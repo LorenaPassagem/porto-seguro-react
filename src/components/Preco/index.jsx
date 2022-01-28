88b07fe7e78f26970952658e570bf5dca32f53d7
lorenapassagem <lorenapassagem@gmail.com>
2022-01-28 11:10:44 -0300

    criar o novo components Preco

--- src/components/Preco/index.jsx
+++ src/components/Preco/index.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import './index.css'
 
 function Preco(){
- return <p className="preco">A partir de R$<span className="precoSpan"> 255</span>,65*</p>
+ return (
+  <>
+    <p className="preco">A partir de<span className="dolar">R$</span><span className="precoSpan"> 255</span>,65*</p>
+    <p className="info">*Valor referente ao plano Ouro Copar Q (ANS: 481.277/18-8) de 3 à 9 vidas, com 20% de copart. com faixa etária entre 0 e 18 anos. Sem IOF. Promoção válida até 30 de abril de 2021.</p>
+  </> 
+ )
 }
 export default Preco
